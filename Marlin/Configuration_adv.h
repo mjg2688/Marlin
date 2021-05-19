ec4843018b77300cba5a3aa412d1894f43b82960
mjg2688 <65995271+mjg2688@users.noreply.github.com>
2021-05-18 19:56:47 -0700

    Merge remote-tracking branch 'upstream/bugfix-2.0.x' into bugfix-2.0.x

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -2495,7 +2495,7 @@
   #define INTERPOLATE      true
 
   #if AXIS_IS_TMC(X)
-    #define X_CURRENT       650       // (mA) RMS current. Multiply by 1.414 for peak current.
+    #define X_CURRENT       700       // (mA) RMS current. Multiply by 1.414 for peak current.
     #define X_CURRENT_HOME  (X_CURRENT/2)  // (mA) RMS current for sensorless homing
     #define X_MICROSTEPS     16        // 0..256
     #define X_RSENSE          0.11
@@ -2513,7 +2513,7 @@
   #endif
 
   #if AXIS_IS_TMC(Y)
-    #define Y_CURRENT       650
+    #define Y_CURRENT       700
     #define Y_CURRENT_HOME  (Y_CURRENT/2)
     #define Y_MICROSTEPS     16
     #define Y_RSENSE          0.11
